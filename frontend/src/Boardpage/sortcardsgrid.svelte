c8ae5d56ca0fbc29c2c2b15ef0af0321de25510e
gechternacht <gabriel@agenciavinci.com.br>
2024-04-17 11:02:03 -0300

    multidrag removed for now

--- frontend/src/Boardpage/sortcardsgrid.svelte
+++ frontend/src/Boardpage/sortcardsgrid.svelte
@@ -1,99 +1,99 @@
 <script>
-// @ts-nocheck
+    // @ts-nocheck
 
-	import { onDestroy, onMount } from "svelte";
-    import Sortable from "sortablejs/modular/sortable.complete.esm.js"
+    import { onDestroy, onMount } from "svelte";
+    import Sortable from "sortablejs";
     import { createEventDispatcher } from "svelte";
-	
-    
+
     export let options = {};
 
-	let sortableElement,sortableObj;
+    let sortableElement, sortableObj;
 
-    const dispatch = createEventDispatcher()
-    
+    const dispatch = createEventDispatcher();
 
     const defaults = {
-            scroll: true,
-            forceAutoScrollFallback: true,
-            forceFallback: false, 
+        scroll: true,
+        forceAutoScrollFallback: true,
+        forceFallback: false,
 
-            scrollSensitivity: 70,
-            bubbleScroll: true,
-            direction: 'vertical',
-			group: {
-				name: 'cards'
-			},
-            handle: ".grabber",
-			animation: 0,
-            swapThreshold: 0.30,
-            multiDrag: true,
-            selectedClass: 'selected',
-            delay: 300, // time in milliseconds to define when the sorting should start
-	        delayOnTouchOnly: true,
-            ghostClass: "sortable-ghost",
-            chosenClass: "sortable-chosen",  // Class name for the chosen item
-	        dragClass: "sortable-drag",
-            onStart: e =>{
-                console.log(e.item)
-                // e.item.draggable = false
-                document.querySelector("body").classList.add("elementisbeingdragged")
-            },
-            onEnd: e =>{
-                // console.log("in the end")
-                
-                document.querySelector("body").classList.remove("elementisbeingdragged")
-            },
-            onUpdate: e => {
-                // console.log(e)
-                const tochildren = Array.prototype.slice.call(e.target.children).map(e => e.id);
-		        console.log("updated!")
-                dispatch("change",{tochildren:tochildren})
-	        },
-            onAdd: e =>{
-                
-                // console.log(e)
-                const tochildren = Array.prototype.slice.call(e.target.children).map(e => e.id);
-                // console.log("im the added one")
-                // console.log(fromchildren,tochildren)
-                
-                dispatch("change",{tochildren:tochildren})
-                
-            },
-            onRemove: e => {
-		    // same properties as onEnd
+        scrollSensitivity: 70,
+        bubbleScroll: true,
+        direction: "vertical",
+        group: {
+            name: "cards",
+        },
+        handle: ".grabber",
+        animation: 0,
+        swapThreshold: 0.3,
+        multiDrag: false,
+        selectedClass: "selected",
+        delay: 300, // time in milliseconds to define when the sorting should start
+        delayOnTouchOnly: true,
+        ghostClass: "sortable-ghost",
+        chosenClass: "sortable-chosen", // Class name for the chosen item
+        dragClass: "sortable-drag",
+        onStart: (e) => {
+            console.log(e.item);
+            // e.item.draggable = false
+            document
+                .querySelector("body")
+                .classList.add("elementisbeingdragged");
+        },
+        onEnd: (e) => {
+            // console.log("in the end")
+
+            document
+                .querySelector("body")
+                .classList.remove("elementisbeingdragged");
+        },
+        onUpdate: (e) => {
             // console.log(e)
-                const tochildren = Array.prototype.slice.call(e.target.children).map(e => e.id);
-                // console.log("im the removed one")
-                // console.log(fromchildren,tochildren)
-                
-                dispatch("change",{tochildren:tochildren})
-	    },
-        ...options
-}
+            const tochildren = Array.prototype.slice
+                .call(e.target.children)
+                .map((e) => e.id);
+            console.log("updated!");
+            dispatch("change", { tochildren: tochildren });
+        },
+        onAdd: (e) => {
+            // console.log(e)
+            const tochildren = Array.prototype.slice
+                .call(e.target.children)
+                .map((e) => e.id);
+            // console.log("im the added one")
+            // console.log(fromchildren,tochildren)
 
-	onMount(()=> {
-            sortableObj = new Sortable(sortableElement, defaults);
-        
-            console.log(sortableObj)
+            dispatch("change", { tochildren: tochildren });
+        },
+        onRemove: (e) => {
+            // same properties as onEnd
+            // console.log(e)
+            const tochildren = Array.prototype.slice
+                .call(e.target.children)
+                .map((e) => e.id);
+            // console.log("im the removed one")
+            // console.log(fromchildren,tochildren)
 
+            dispatch("change", { tochildren: tochildren });
+        },
+        ...options,
+    };
 
-	});
+    onMount(() => {
+        sortableObj = new Sortable(sortableElement, defaults);
 
+        console.log(sortableObj);
+    });
 
-    onDestroy( ()=>{
-        sortableObj.destroy()
-    })
-
-
+    onDestroy(() => {
+        sortableObj.destroy();
+    });
 </script>
 
 <main>
-
-    <div class="{$$restProps.class || 'sortableElement'}" bind:this={sortableElement}>
-
-        <slot></slot>
-    
+    <div
+        class={$$restProps.class || "sortableElement"}
+        bind:this={sortableElement}
+    >
+        <slot />
     </div>
 </main>
-
