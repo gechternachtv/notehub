22725cc373486888f95e3e82f4fc6c1c6d46db3f
gechternacht <gabriel@agenciavinci.com.br>
2024-10-30 10:57:45 -0300

    tag fix

--- frontend/src/workspacepage/workspacepage.svelte
+++ frontend/src/workspacepage/workspacepage.svelte
@@ -115,10 +115,10 @@
             expand: "usergroup,boards.cards.tags",
             fields:
                 `collectionId,boards,grid,id,img,name,imgposition,usergroup,expand.usergroup.users,expand.usergroup.name,` +
-                `expand.boards.id,expand.boards.img,expand.boards.name,expand.boards.cards,expand.boards.color,expand.boards.collectionId,` +
+                `expand.boards.id,expand.boards.usergroup,expand.boards.img,expand.boards.name,expand.boards.cards,expand.boards.color,expand.boards.collectionId,` +
                 `expand.boards.expand.cards.title,expand.boards.expand.cards.collectionId,expand.boards.expand.cards.check,expand.boards.expand.cards.created,` +
                 `expand.boards.expand.cards.id,expand.boards.expand.cards.color,expand.boards.expand.cards.file,expand.boards.expand.cards.imglink,expand.boards.expand.cards.favico,` +
-                `expand.boards.expand.cards.link,expand.boards.expand.cards.expand.tags.name,expand.boards.expand.cards.expand.tags.id,expand.boards.expand.cards.expand.tags.color,expand.boards.expand.cards.text,expand.boards.expand.cards.expand.tags.usergroup`,
+                `expand.boards.expand.cards.link,expand.boards.expand.cards.expand.tags.name,expand.boards.expand.cards.expand.tags.usergroup,expand.boards.expand.cards.expand.tags.id,expand.boards.expand.cards.expand.tags.color,expand.boards.expand.cards.text,expand.boards.expand.cards.expand.tags.usergroup`,
         });
         console.log("%c ====>", "color:teal;font-size:40px");
         console.log(record);
